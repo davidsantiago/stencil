9a4265beaefae2eb23279c529fb3bbf46544bfa2
David Santiago <david.santiago@gmail.com>
2011-06-27 20:32:08 -0500

    Update swank-clj version.

--- project.clj
+++ project.clj
@@ -2,6 +2,6 @@
   :description "Mustache in Clojure"
   :dependencies [[clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]]
-  :dev-dependencies [[swank-clj "0.1.6-SNAPSHOT"]
+  :dev-dependencies [[swank-clj "0.1.7-SNAPSHOT"]
                      [org.clojure/clojure-contrib "1.2.0"]]
   :tasks [cake.tasks.swank-clj stencil.cake.tasks])
