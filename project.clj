db691c67215fcbf350cfe79334e62bf9f3177b41
David Santiago <david.santiago@gmail.com>
2012-02-19 01:46:16 -0800

    Add Clojure 1.4.0-beta1 to lein-multi deps.

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
                  [slingshot "0.8.0"]]
   :multi-deps {"1.2" [[org.clojure/clojure "1.2.1"]]
                "1.3" [[org.clojure/clojure "1.3.0"]]
+               "1.4" [[org.clojure/clojure "1.4.0-beta1"]]
                :all [[slingshot "0.8.0"]]}
   :dev-dependencies [[ritz "0.2.0"]
                      [org.clojure/data.json "0.1.1"]]
