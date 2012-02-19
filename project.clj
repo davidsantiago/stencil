9499074ed798f6b5ae5efd2ee83588006955e145
David Santiago <david.santiago@gmail.com>
2012-02-19 01:44:28 -0800

    Update project.clj to use lein multi on for testing on Clojure 1.2.1 and 1.3.

--- project.clj
+++ project.clj
@@ -2,6 +2,9 @@
   :description "Mustache in Clojure"
   :dependencies [[clojure "1.3.0"]
                  [slingshot "0.8.0"]]
+  :multi-deps {"1.2" [[org.clojure/clojure "1.2.1"]]
+               "1.3" [[org.clojure/clojure "1.3.0"]]
+               :all [[slingshot "0.8.0"]]}
   :dev-dependencies [[ritz "0.2.0"]
                      [org.clojure/data.json "0.1.1"]]
   :extra-files-to-clean ["test/spec"])
