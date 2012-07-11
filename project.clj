94cc45c58a7d52d88b7cbb7a6a33af981b4ac5c8
David Santiago <david.santiago@gmail.com>
2012-07-11 12:34:37 -0700

    Update some dependencies.

--- project.clj
+++ project.clj
@@ -1,11 +1,14 @@
 (defproject stencil "0.3.0"
   :description "Mustache in Clojure"
-  :dependencies [[clojure "1.3.0"]
-                 [slingshot "0.8.0"]]
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [slingshot "0.8.0"]
+                 [org.clojure/core.cache "0.5.0"]]
   :multi-deps {"1.2" [[org.clojure/clojure "1.2.1"]]
                "1.3" [[org.clojure/clojure "1.3.0"]]
                "1.4" [[org.clojure/clojure "1.4.0-beta1"]]
-               :all [[slingshot "0.8.0"]]}
+               :all [[slingshot "0.8.0"]
+                     [org.clojure/core.cache]]}
   :dev-dependencies [[ritz "0.2.0"]
-                     [org.clojure/data.json "0.1.1"]]
+                     [org.clojure/data.json "0.1.2"]]
+  :profiles {:dev {:dependencies [[org.clojure/data.json "0.1.2"]]}}
   :extra-files-to-clean ["test/spec"])
