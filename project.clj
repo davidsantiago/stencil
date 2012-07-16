8346a8ade16f342f425e74e77c6eb3ede5737b92
David Santiago <david.santiago@gmail.com>
2012-07-16 02:21:31 -0700

    Change core.cache version to 0.6.1.

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [scout "0.1.0"]
                  [slingshot "0.8.0"]
-                 [org.clojure/core.cache "0.5.0"]]
+                 [org.clojure/core.cache "0.6.1"]]
   :profiles {:dev {:dependencies [[org.clojure/data.json "0.1.2"]]}
              :clj1.2 {:dependencies [[org.clojure/clojure "1.2.1"]]}
              :clj1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
