f549a0c25ebd977dd660b9ca2e8bd23a908ea5ef
David Santiago <david.santiago@gmail.com>
2012-07-19 01:18:22 -0700

    Add Clojure 1.5 profile and repositories.

--- project.clj
+++ project.clj
@@ -8,5 +8,12 @@
   :profiles {:dev {:dependencies [[org.clojure/data.json "0.1.2"]]}
              :clj1.2 {:dependencies [[org.clojure/clojure "1.2.1"]]}
              :clj1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
-             :clj1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}}
-  :extra-files-to-clean ["test/spec"])
+             :clj1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
+             :clj1.5 {:dependencies [[org.clojure/clojure "1.5.0-master-SNAPSHOT"]]}}
+  :aliases {"all" ["with-profile" "dev:dev,clj1.4:dev,clj1.5"]}
+  :repositories {"sonatype" {:url "http://oss.sonatype.org/content/repositories/releases"
+                             :snapshots false
+                             :releases {:checksum :fail :update :always}}
+                 "sonatype-snapshots" {:url "http://oss.sonatype.org/content/repositories/snapshots"
+                                       :snapshots true
+                                       :releases {:checksum :fail :update :always}}})
