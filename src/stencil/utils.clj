2539481ac8a7d885e66c597def0ce9966c1e5bfd
David Santiago <david.santiago@gmail.com>
2012-07-18 23:30:08 -0700

    Move html-escape and indent-string from utils to quoin.text. Associated tests too.

--- src/stencil/utils.clj
+++ src/stencil/utils.clj
@@ -1,43 +1,6 @@
 (ns stencil.utils
   (:require [clojure.string :as str]))
 
-(defn html-escape
-  "HTML-escapes the given string."
-  [^String s]
-  ;; This method is "Java in Clojure" for serious speedups.
-  (let [sb (StringBuilder.)
-        slength (long (count s))]
-    (loop [idx (long 0)]
-      (if (>= idx slength)
-        (.toString sb)
-        (let [c (char (.charAt s idx))]
-          (case c
-            \& (.append sb "&amp;")
-            \< (.append sb "&lt;")
-            \> (.append sb "&gt;")
-            \" (.append sb "&quot;")
-            (.append sb c))
-          (recur (inc idx)))))))
-
-(defn indent-string
-  "Given a String s, indents each line by inserting the string indentation
-   at the beginning."
-  [^String s ^String indentation]
-  (let [str+padding (StringBuilder.)]
-    (loop [start-idx 0
-           next-idx (.indexOf s "\n")] ;; \n handles both \r\n & \n linebreaks.
-      (if (= -1 next-idx)
-        ;; We've reached the end. If the start and end are the same, don't
-        ;; indent before an empty string. Either way, return the string. 
-        (do (when (not= start-idx (count s))
-              (.append str+padding indentation)
-              (.append str+padding s start-idx (count s)))
-            (.toString str+padding))
-        (let [next-idx (inc next-idx)]
-          (.append str+padding indentation)
-          (.append str+padding s start-idx next-idx)
-          (recur next-idx (.indexOf s "\n" next-idx)))))))
-
 ;;
 ;; Fuzzy map access routines
 ;;
--- test/stencil/test/utils.clj
+++ test/stencil/test/utils.clj
@@ -2,25 +2,6 @@
   (:use clojure.test
         stencil.utils))
 
-(deftest test-html-escape
-  (is (= "&lt;script&gt;"
-         (html-escape "<script>")))
-  (is (= "&amp;lt;script&amp;gt;"
-         (html-escape (html-escape "<script>"))))
-  (is (= "&lt;script src=&quot;blah.js&quot;&gt;"
-         (html-escape "<script src=\"blah.js\">"))))
-
-(deftest test-indent-string
-  (is (= " blah\n blah")
-      (indent-string "blah\nblah" " "))
-  (is (= " blah\r\n blah"
-         (indent-string "blah\r\nblah" " ")))
-  (is (= " blah"
-         (indent-string "blah" " ")))
-  ;; Shouldn't indent a non-existing last line when string ends on \n.
-  (is (= " blah\n"
-         (indent-string "blah\n" " "))))
-
 (deftest test-contains-fuzzy?
   (is (= :a
          (contains-fuzzy? {:a 1} :a)))
