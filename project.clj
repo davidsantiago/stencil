adc3fafac638960c954b3fc6e1ead39807d76f6c
David Santiago <david.santiago@gmail.com>
2012-08-14 16:34:20 -0700

    Update to core.cache 0.6.2.

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
                  [scout "0.1.0"]
                  [quoin "0.1.0"]
                  [slingshot "0.8.0"]
-                 [org.clojure/core.cache "0.6.1"]]
+                 [org.clojure/core.cache "0.6.2"]]
   :profiles {:dev {:dependencies [[org.clojure/data.json "0.1.2"]]}
              :clj1.2 {:dependencies [[org.clojure/clojure "1.2.1"]]}
              :clj1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
