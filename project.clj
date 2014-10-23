187c814a946aa0f6da4cdc671581541a4a369fea
David Santiago <david.santiago@gmail.com>
2014-10-23 10:53:33 -0700

    Update project version for further development.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject stencil "0.3.5"
+(defproject stencil "0.3.6-SNAPSHOT"
   :description "Mustache in Clojure"
   :url "https://github.com/davidsantiago/stencil"
   :dependencies [[org.clojure/clojure "1.3.0"]
